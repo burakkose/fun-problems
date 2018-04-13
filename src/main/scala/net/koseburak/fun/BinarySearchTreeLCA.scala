e75488f77bf6f894183aa16801f5f9998c997535
Burak KOSE <burakks41@gmail.com>
2018-04-13 10:37:31 +0200

    This should be object

--- src/main/scala/net/koseburak/fun/BinarySearchTreeLCA.scala
+++ src/main/scala/net/koseburak/fun/BinarySearchTreeLCA.scala
@@ -2,11 +2,10 @@ package net.koseburak.fun
 
 import scala.annotation.tailrec
 
-class BinarySearchTreeLCA {
+object BinarySearchTreeLCA {
   sealed trait Tree
   case class Node(value: Int, left: Tree = Nil, right: Tree = Nil) extends Tree
   case object Nil extends Tree
-
 
   def lca(tree: Tree, n1: Int, n2: Int): Tree = {
     @tailrec
@@ -14,9 +13,9 @@ class BinarySearchTreeLCA {
       t match {
         case Nil => Nil
         case Node(data, l, r) =>
-          if(data > n1 && data > n2)
+          if (data > n1 && data > n2)
             inner(l)
-          else if(data < n1 && data < n2)
+          else if (data < n1 && data < n2)
             inner(r)
           else t
       }
@@ -25,10 +24,8 @@ class BinarySearchTreeLCA {
   }
 
   val tree =
-    Node(20,
-      Node(8, Node(4), Node(12, Node(10), Node(14))),
-      Node(22))
+    Node(20, Node(8, Node(4), Node(12, Node(10), Node(14))), Node(22))
 
-  lca(tree, 14,8)
+  lca(tree, 14, 8)
 
 }
